3015385e1984d35d61043865d2b81e89743d1f5c
Niks2333 <nikhilvc.143@gmail.com>
2025-08-06 19:49:54 +0530

    06/08

--- InventoryManagementBackend/Controllers/StoreController.cs
+++ InventoryManagementBackend/Controllers/StoreController.cs
@@ -12,14 +12,15 @@ namespace InventoryManagementBackend.Controllers
     [RoutePrefix("api/store")]
     public class StoreController : ApiController
     {
+        private readonly StoreRepository repo = new StoreRepository();
+
+       
         [HttpGet]
         [Route("list")]
         public IHttpActionResult GetStores()
         {
             try
-            {   
-
-                var repo = new StoreRepository();
+            {
                 var stores = repo.GetStores();
                 return Ok(stores);
             }
@@ -29,6 +30,40 @@ namespace InventoryManagementBackend.Controllers
                 return InternalServerError(ex);
             }
         }
-    }
 
+
+      
+        [HttpGet]
+        [Route("productlist")]
+        public IHttpActionResult GetAllProducts()
+        {
+            try
+            {
+                var products = repo.GetAllProducts2();
+                return Ok(products);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex.Message, ex.StackTrace, 0);
+                return InternalServerError(ex);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("storetypes")]
+        public IHttpActionResult GetStoreTypes()
+        {
+            try
+            {
+                var types = repo.GetActiveStoreTypes();
+                return Ok(types);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException(ex.Message, ex.StackTrace, 0);
+                return InternalServerError(ex);
+            }
+        }
+    }
 }
